9784666d11690f237a682e876621dcbe3f4f553c
dorichan <dori.p.chan@gmail.com>
2015-02-28 22:35:20 -0800

    -Minor changes

--- Assets/Scripts/ToyAutoFire.cs
+++ Assets/Scripts/ToyAutoFire.cs
@@ -5,14 +5,18 @@ using System.Collections.Generic;
 public class ToyAutoFire : MonoBehaviour 
 {
 	public bool isAttacking;
+	private ToyAI ta;
 
 	void Start()
 	{
 		isAttacking = false;
+		ta = GetComponent<ToyAI> ();
 	}
 
 	void Update()
 	{
-
+		if (ta.enemy != null) {
+			Debug.Log("Pew Pew Pew!");
+		}
 	}
 }
